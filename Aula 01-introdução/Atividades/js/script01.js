6b96fb97259fd55cdda13b9ed9b9657e6465f54a
Thainara Paulo <66797825+Thainarapaulo@users.noreply.github.com>
2023-11-21 11:51:24 -0300

    Atividade 01
    JS atividades

---
+++ Aula 01-introdução/Atividades/js/script01.js
@@ -0,0 +1,23 @@
+alert("Calcule a Conta");
+var numeroClientes = Number(prompt("Número de clientes na mesa"));
+var valorTotalConta = Number(prompt("Digite o valor da conta"));
+ var divisao = valorTotalConta / numeroClientes;
+ alert(` A divisão da conta é de R$ ${divisao}`);
+
+// FUNÇÃO
+//  function calculeConta(){
+//  alert("Calcule a Conta");
+// let numeroClientes = Number(prompt("Número de clientes na mesa"));
+// let valorTotalConta = Number(prompt("Digite o valor da conta"));
+//  let divisao = valorTotalConta / numeroClientes;
+//  alert(` A divisão da conta é de R$ ${divisao}`);
+//  }
+
+// VALOR FIXO
+//  function calculeConta(numeroClientes, valorTotalConta){
+//  alert("Calcule a Conta");
+
+//  let divisao = valorTotalConta / numeroClientes;
+//  alert(` A divisão da conta é de R$ ${divisao}`);
+// calculeConta ();
+//  }
---
+++ Aula 01-introdução/Atividades/js/script02.js
@@ -0,0 +1,6 @@
+alert("Sucessor e Antecessor");
+
+var numero = Number(prompt("Digite um número"));
+var sucessor = numero + 1;
+var antecessor = numero1 - 1;
+alert(`O número antecessor é:${numero - 1}, o número escolhido:${numero}, o número sucessor é:${numero + 1}`);
