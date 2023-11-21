1df5d6778cb93e1ef6a31759afa5c44a1c27550d
Thainara Paulo <66797825+Thainarapaulo@users.noreply.github.com>
2023-11-21 10:53:24 -0300

    Aula introdutoria de JS
    Uso de variaveis, caixas de texto e alert

--- Aula 01-introdução/Material/js/script02.js
+++ Aula 01-introdução/Material/js/script02.js
@@ -7,3 +7,13 @@ alert("Calculadora de soma");
 var soma = numero1 + numero2;
 
  alert(` O resultado da soma é de ${soma}`);
+
+//  operadores aritméticos
+// + somar (concatenar)
+// - subtrair
+// * multiplicar
+// ** elevar um número
+// / dividir
+// = atribuir
+// ++ somar mais a um ao número
+// -- subtrair um ao número
