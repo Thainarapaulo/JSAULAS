6bf5395170a39f62f592807cf3f3fa55dfa29d26
Thainara Paulo <thainaradepaulo@gmail.com>
2023-11-27 20:39:59 -0300

    Atividade Carro
    Carro JavaScript

--- Aula 05-Objeto JS/atividade/js/script.js
+++ Aula 05-Objeto JS/atividade/js/script.js
@@ -4,18 +4,28 @@ class Carro{
     ano;
     cor;
     velocidadeMaxima; 
-    VelocidadeAtual;
-    acelerar(valor){
-      if(numero > VelocidadeAtual){
-        
-      }
-    };
+    velocidadeAtual;
+    acelerar(numero){
+      if( numero>= 0){
+        var novaVelocidade = this.velocidadeAtual + numero;
+        if (novaVelocidade <= this.velocidadeMaxima) {
+            this.velocidadeAtual = novaVelocidade;
+            return "Velocidade atual: " + this.velocidadeAtual;
+        } else {
+            this.velocidadeAtual = this.velocidadeMaxima;
+            return "Ultrapassou o limite máximo. Velocidade atual: " + this.velocidadeAtual;
+        }
+    } else {
+        return "Aceleração inválida. O número deve ser positivo.";
+    }
 }
+        
+      };
 
 var carro1 = new Carro ();
-   this.marca ="PEGEOUT"
-   this.modelo = "PEGEOUT 208";
-   this.ano = 2010;
-   this.cor = "PRATA";
-   this.velocidadeMaxima = 80; 
-   this.VelocidadeAtual = 0;
+   carro1.marca ="PEGEOUT"
+   carro1.modelo = "PEGEOUT 208";
+   carro1.ano = 2010;
+   carro1.cor = "PRATA";
+   carro1.velocidadeMaxima = 80; 
+   carro1.VelocidadeAtual = 40;
