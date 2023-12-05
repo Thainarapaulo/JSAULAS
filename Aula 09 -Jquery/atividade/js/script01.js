f1ffc1d9a967655da1f7de21b320cfed3444130e
Thainara Paulo <66797825+Thainarapaulo@users.noreply.github.com>
2023-12-05 09:19:35 -0300

    Atividade 09
    Uso do Jquery

---
+++ Aula 09 -Jquery/atividade/js/script01.js
@@ -0,0 +1,9 @@
+// addEve.... = on
+$("#btnCalcular").on("click", function(){
+    let numeroEscolhido = Number($("#numeroEscolhido").val());
+    for(let i = 1; i <= 10; i++){
+        document.querySelector("#resultadoCalculo").innerHTML += `${i} x ${numeroEscolhido} =${i*numeroEscolhido} <br>`;
+
+    }
+});
+
